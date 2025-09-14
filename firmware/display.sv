3f1fef0597102b5e3476d71fb9798e2a37a6759c
Julien <julien@rabidaudio.com>
2025-09-14 18:37:51 -0400

    add parameters

--- firmware/display.sv
+++ firmware/display.sv
@@ -1,7 +1,8 @@
 `default_nettype none
 `timescale 1ns/10ps
 
-// typedef enum { MSB, LSB } bit_order_e;
+typedef enum { MSB, LSB } bit_order_e;
+typedef enum { ACTIVE_LOW = 0, ACTIVE_HIGH = 1 } pin_direction_e;
 
 /**
   * The SpiSlaveSimplex module is an SPI slave
@@ -11,34 +12,45 @@
   * The clock is expected to be an external input
   * from the master device, so the system clock
   * will need to be greater than master_clk / 8.
+  * byte_index will indicate the current byte
+  * number since the start of the transaction.
   */
-//  #(
-//     parameter ORDER bit_order_e = MSB,
-//     parameter CS_DIRECTION active_low,
-// )
-module SpiSlaveSimplex (
+module SpiSlaveSimplex #(
+    parameter ORDER = MSB,
+    parameter CS_DIRECTION = ACTIVE_LOW,
+    parameter BYTE_INDEX_SIZE = 11 // roll over at 2048 bytes
+)(
     input master_clk,
     input cs,
     input mosi,
     output logic [7:0] data,
-    output logic data_ready
+    output logic data_ready,
+    output logic [BYTE_INDEX_SIZE:0] byte_index
 );
 
     logic [2:0] bit_index;
 
     always_ff @(posedge master_clk)
-        if (cs == 1) begin
+        if (cs == CS_DIRECTION) begin
+            // else load bit into data
+            if (ORDER == MSB) data <= { data[6:0], mosi };
+            else data <= { mosi, data[7:1] };
+
+            if (bit_index == 7) begin
+                data_ready <= 1;
+                byte_index <= byte_index + 1;
+            end else begin
+                data_ready <= 0;
+                byte_index <= byte_index;
+            end
+            
+            bit_index <= bit_index + 1;
+        end else begin
             // if cs disabled, reset
             data <= 0;
             data_ready <= 0;
             bit_index <= 0;
-        end else begin
-            // else load bit into data
-            data <= { data[6:0], mosi };
-            if (bit_index == 7) data_ready <= 1;
-            else data_ready <= 0;
-            
-            bit_index <= bit_index + 1;
+            byte_index <= 0;
         end
 endmodule
 
@@ -67,13 +79,15 @@ module Test_SpiSlaveSimplex;
     logic test_mosi;
     logic [7:0] test_data_out;
     logic test_data_ready;
+    logic [5:0] test_byte_index;
 
-    SpiSlaveSimplex test_spi_slave_simplex(
+    SpiSlaveSimplex #(.BYTE_INDEX_SIZE(5)) test_spi_slave_simplex(
         .master_clk(test_spi_clk),
         .cs(test_cs),
         .mosi(test_mosi),
         .data(test_data_out),
-        .data_ready(test_data_ready)
+        .data_ready(test_data_ready),
+        .byte_index(test_byte_index)
     );
 
     logic [7:0] test_data_in;
